4a9d3715a52ac0309e4bee6318252802baa3c4d6
unknown <rezadadashian@yahoo.ca>
2020-02-03 23:55:50 +0330

    PassedSteps Function & some minor bug fixes

--- api/views/projects.py
+++ api/views/projects.py
@@ -1,10 +1,13 @@
+from django.shortcuts import get_object_or_404
 import datetime
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from mysite.estimator.models import Estimate
+from mysite.estimator.models import *
+from mysite.order.models import *
+from mysite.gi.models import *
 
 from ..models.project import ProjectModel
 from ..serializers.project import ProjectSerializer
@@ -24,8 +27,9 @@ class ProjectsAPIView(APIView):
 
             def map_func(result_page):
                 items = map((lambda estimate: (estimate.project,
-                                               estimate.customer,
-                                               estimate.engineer)),
+                                               estimate.created_by.profile.user.email,
+                                               estimate.engineer,
+                                               project_step_tracer(estimate))),
                             result_page)
                 projects = map((lambda item:
                                 ProjectModel(
@@ -36,9 +40,9 @@ class ProjectsAPIView(APIView):
                                     item[0].state,
                                     item[0].zip,
                                     item[0].created_on,
-                                    item[1].name,
+                                    item[1],
                                     item[2].name,
-                                    0)
+                                    item[3])
                                 ),
                                items)
                 return projects
@@ -75,3 +79,15 @@ class ProjectsAPIView(APIView):
                 ('' if filter['asc'] else '-') + 'project__' + filter['ordering'])
 
         return estimates
+
+
+def project_step_tracer(estimate):
+    if Invoice.objects.filter(order__proposal__quote__estimate__id=estimate.id).exists():
+        return 5
+    elif Order.objects.filter(proposal__quote__estimate__id=estimate.id).exists():
+        return 4
+    elif Proposal.objects.filter(quote__estimate__id=estimate.id).exists():
+        return 3
+    elif Quote.objects.filter(estimate__id=estimate.id).exists():
+        return 2
+    return 1
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from mysite.core import views as core_views
 
