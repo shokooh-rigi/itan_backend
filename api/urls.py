ba183882bc22a2cf1644ca4b614a84dcf7fb284b
Unes <unes1374@gmail.com>
2020-02-05 23:46:42 +0330

    متد put را در UsersApiView برای ویرایش user و کلمه عبور آن نوشتم.

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,7 @@ from .views import views
 urlpatterns = [
     path('projects/', views.ProjectsAPIView.as_view()),
     path('session/', views.SessionAPIView.as_view()),
+    path('users/', views.UsersAPIView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views/views.py
+++ api/views/views.py
@@ -1,2 +1,3 @@
 from .projects import ProjectsAPIView
 from .session import SessionAPIView
+from .users import UsersAPIView
