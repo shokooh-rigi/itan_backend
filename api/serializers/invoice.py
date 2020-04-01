b27d4e916530913209541c9fcc5c4715f16bd390
Unes <unes1374@gmail.com>
2020-04-01 15:58:08 +0430

    API: in InvoiceSerializer, necessary fields added. in InvoicesAPIView, pagination added for GET method.

--- api/serializers/invoice.py
+++ api/serializers/invoice.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
 
+from mysite.gi.views import estimate_total_calculator, calculate_total_amount_due
 from mysite.gi.models import Invoice
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
+    invoice_number = serializers.SerializerMethodField()
+    project_number = serializers.CharField(source='order.project_number')
+    project_name = serializers.CharField(
+        source='order.proposal.quote.estimate.bfm.project.name')
+    total_ordered = serializers.SerializerMethodField()
+    total_invoiced = serializers.SerializerMethodField()
+
     class Meta:
         model = Invoice
-        fields = "__all__"
+        fields = [
+            'id',
+            'invoice_number',
+            'project_number',
+            'project_name',
+            'total_ordered',
+            'percent_of_performance_completed',
+            'total_payment_received_to_date',
+            'past_due_amount',
+            'total_invoiced',
+            'created_on',
+        ]
+
+    def get_invoice_number(self, invoice):
+        return '%s-%03d' % (invoice.order.project_number[3:], invoice.id)
+
+    def get_total_ordered(self, invoice):
+        return estimate_total_calculator(invoice.order.proposal.quote.estimate.id)
+
+    def get_total_invoiced(self, invoice):
+        return calculate_total_amount_due(invoice)
--- api/views/invoices.py
+++ api/views/invoices.py
@@ -1,21 +1,55 @@
+import datetime
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework import status
+
+from mysite.gi.models import Invoice
 
 from ..authentication_mixin import AuthenticationMixin
 from ..serializers.invoice import InvoiceSerializer
-from mysite.gi.models import Invoice
+from ..pagination import CustomPageNumberPagination
 
 
 class InvoicesAPIView(AuthenticationMixin, APIView):
     serializer_class = InvoiceSerializer
 
     def _get_invoice(self, request, pk):
-        return get_object_or_404(Invoice, pk=pk, mark_as_paid=False, order__proposal__quote__estimate__bfm__customer=request.user.profile.customer)
+        return get_object_or_404(self._get_all_invoices(request), pk=pk)
 
     def _get_all_invoices(self, request):
         return Invoice.objects.filter(mark_as_paid=False, order__proposal__quote__estimate__bfm__customer=request.user.profile.customer)
+
+    def _filter_invoices(self, request, invoices):
+        """Filter the invoices"""
+
+        # filter = {
+        #     'search': request.query_params.get('search', ''),
+        #     'fromDate': request.query_params.get('fromDate', ''),
+        #     'toDate': request.query_params.get('toDate', ''),
+        #     'ordering': request.query_params.get('ordering', ''),
+        #     'asc': True if request.query_params.get('asc', 'true') == 'true' else False
+        # }
+
+        # if filter['search']:
+        #     invoices = invoices.filter(
+        #         Q(project__name__icontains=filter['search']))
+
+        # if filter['fromDate'] and filter['toDate']:
+        #     from_date_obj = datetime.datetime.strptime(
+        #         filter['fromDate'], '%m/%d/%Y')
+        #     to_date_obj = datetime.datetime.strptime(
+        #         filter['toDate'], '%m/%d/%Y')
+        #     to_date_obj = to_date_obj + \
+        #         datetime.timedelta(hours=23, minutes=59, seconds=59)
+        #     invoices = invoices.filter(
+        #         project__created_on__range=(from_date_obj, to_date_obj))
+
+        # if filter['ordering']:
+        #     invoices = invoices.order_by(
+        #         ('' if filter['asc'] else '-') + 'project__' + filter['ordering'])
+
+        return invoices
 
     def get(self, request, pk=None, format=None):
         """Get the Invoice object/list"""
@@ -26,5 +60,6 @@ class InvoicesAPIView(AuthenticationMixin, APIView):
             return Response(serializer.data)
         else:
             invoices = self._get_all_invoices(request)
-            serializer = self.serializer_class(invoices, many=True)
-            return Response(serializer.data)
+            invoices = self._filter_invoices(request, invoices)
+            paginator = CustomPageNumberPagination()
+            return paginator.get_paginated_response(invoices, request, self.serializer_class)
