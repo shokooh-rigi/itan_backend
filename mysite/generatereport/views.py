61a4e777aacfbfdde37aab633d9e848ee7a5120d
Reza Reev <reza@reev.ca>
2022-05-03 20:04:51 +0300

    - report bug fix

--- mysite/generatereport/views.py
+++ mysite/generatereport/views.py
@@ -244,6 +244,8 @@ def report_sheet_recreate(request, sheet_id):
                     'system': 'PUMP TEST SHEET',
                     'pump_equipment': pump_equipment_data
                 })
+
+            print(len(pump_equipments))
             if len(pump_equipments) > 0:
                 toc_line_maker('PUMP TEST SHEET', total_pages, 0)
 
