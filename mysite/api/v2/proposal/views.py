ac6517e5e078020dd128a6d2463b8893b98016c1
shokooh Rigi <shokoohrigi@yahoo.com>
2024-11-15 15:31:30 +0330

    edit proposal

--- mysite/api/v2/proposal/views.py
+++ mysite/api/v2/proposal/views.py
@@ -249,17 +249,6 @@ class ProposalCreateView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-
-from mysite.proposal.models import Proposal
-
-
 class ProposalArchiveView(APIView):
     """
     Archives a proposal if the user is authorized and confirms the action.
