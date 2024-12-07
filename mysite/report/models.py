5e04db9436d087b412c09c90e7a2c045c9f4c95e
Reza Dadashian <rezadadashian@yahoo.ca>
2024-12-08 00:35:02 +0330

    fix: import User on report app

--- mysite/report/models.py
+++ mysite/report/models.py
@@ -1,5 +1,6 @@
 import os
 
+from custom_user.models import User
 from mysite.order.models import *
 
 
@@ -9,18 +10,23 @@ from mysite.order.models import *
 class Report(models.Model):
     def _(self, filename):
         return os.path.join(
-            r'ReportFiles/{date}/{file}'.format(date=str(self.report_date).replace('/', '_'), file=filename))
+            r"ReportFiles/{date}/{file}".format(
+                date=str(self.report_date).replace("/", "_"), file=filename
+            )
+        )
 
     order = models.OneToOneField(Order, on_delete=models.CASCADE, blank=False)
-    report_date = models.DateField(default=datetime.datetime.now().strftime("%m/%d/%Y"), blank=False, null=False)
+    report_date = models.DateField(
+        default=datetime.datetime.now().strftime("%m/%d/%Y"), blank=False, null=False
+    )
     report_file = models.FileField(upload_to=_, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
 
     class Meta:
         ordering = ["-created_on"]
-        verbose_name = 'Report List'
-        verbose_name_plural = 'Report List'
+        verbose_name = "Report List"
+        verbose_name_plural = "Report List"
 
     def __str__(self):
         return self.order.project_number
