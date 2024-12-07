78bfa19d544152e3bf626c4d3976df28717c601a
Reza Dadashian <rezadadashian@yahoo.ca>
2024-12-08 00:38:36 +0330

    fix: importing invalid model in order_tags.py

--- mysite/order/templatetags/order_tags.py
+++ mysite/order/templatetags/order_tags.py
@@ -1,7 +1,11 @@
 from django import template
 
-from mysite.estimator.templatetags.estimator_tags import estimate_total_calculator, estimate_predemo_calculator, estimate_sub_total_dalt_calculator
-from ..models import ChangeOrder, EstimateEquipment, ChangeOrderService
+from mysite.estimator.templatetags.estimator_tags import (
+    estimate_total_calculator,
+    estimate_predemo_calculator,
+    estimate_sub_total_dalt_calculator,
+)
+from ..models import ChangeOrder, ChangeOrderService
 from ...gi.models import InvoiceTransaction, InvoiceHistory, Setting, Invoice
 from ...scheduler.models import Schedule, Maintenance
 import datetime
@@ -73,15 +77,23 @@ def order_tech_final_price_calculator(estimate_id, order):
 @register.simple_tag
 def calculate_total_amount_due(invoice):
     if invoice.invoice_type == 1:
-        sub_total = order_total_calculator(invoice.order.proposal.estimate.id, invoice.order)
+        sub_total = order_total_calculator(
+            invoice.order.proposal.estimate.id, invoice.order
+        )
     elif invoice.invoice_type == 2:
-        sub_total = order_predemo_calculator(invoice.order.proposal.estimate.id, invoice.order)
+        sub_total = order_predemo_calculator(
+            invoice.order.proposal.estimate.id, invoice.order
+        )
     elif invoice.invoice_type == 4:
-        sub_total = order_dalt_calculator(invoice.order.proposal.estimate.id, invoice.order)
+        sub_total = order_dalt_calculator(
+            invoice.order.proposal.estimate.id, invoice.order
+        )
     else:
-        sub_total = order_final_calculator(invoice.order.proposal.estimate.id, invoice.order)
+        sub_total = order_final_calculator(
+            invoice.order.proposal.estimate.id, invoice.order
+        )
     completed_percentage = invoice.percent_of_performance_completed
-    total = (sub_total * completed_percentage / 100)
+    total = sub_total * completed_percentage / 100
     return total
 
 
@@ -108,23 +120,23 @@ def calculate_remaining_invoice_due(invoice):
 
 @register.simple_tag
 def get_last_invoice_history(invoice):
-    lih = InvoiceHistory.objects.filter(invoice=invoice).order_by('created_on').last
+    lih = InvoiceHistory.objects.filter(invoice=invoice).order_by("created_on").last
     return lih
 
 
 @register.simple_tag
 def get_full_address(project):
-    full_address = ''
+    full_address = ""
     if project.address_line_1:
         full_address += project.address_line_1
     if project.address_line_2:
-        full_address += ' ' + project.address_line_2
+        full_address += " " + project.address_line_2
     if project.city:
-        full_address += ' ' + project.city
+        full_address += " " + project.city
     if project.state:
-        full_address += ' ' + project.state
+        full_address += " " + project.state
     if project.zip:
-        full_address += ' ' + project.zip
+        full_address += " " + project.zip
     return full_address
 
 
@@ -140,7 +152,7 @@ def co_total_amount(co):
 @register.simple_tag
 def past_30_days(order):
     if len(Invoice.objects.filter(order=order)) > 0:
-        overdue_days = Setting.objects.get(key='Overdue Days').value
+        overdue_days = Setting.objects.get(key="Overdue Days").value
         overdue_datetime = timezone.now() - timedelta(days=int(overdue_days))
         created_on_aware = order.invoice.created_on
         if created_on_aware < overdue_datetime:
@@ -152,4 +164,3 @@ def past_30_days(order):
 @register.filter
 def enumerate_list(value):
     return enumerate(value)
-
