75fc8c07a925396fa44956ac87d94ccd8b5fc609
Reza Dadashian <rezadadashian@yahoo.ca>
2024-12-07 23:32:55 +0330

    fix: webhook-handler.sh

--- webhook-handler.sh
+++ webhook-handler.sh
@@ -10,6 +10,7 @@ git pull
 source /home/airdec/virtualenv/public_html/api.airdec.net/3.11/bin/activate
 
 # Run Django management commands
+pip install -r requirements/dev.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
