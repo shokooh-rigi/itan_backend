febaef6191b96a3e869b5479861b367f1c90d1d8
Reza Dadashian <rezadadashian@yahoo.ca>
2024-12-09 16:26:21 +0330

    feat: maintain log for cpanel-updater.php

--- cpanel-updater.php
+++ cpanel-updater.php
@@ -2,9 +2,13 @@
 // Path to your Bash script
 $scriptPath = 'webhook-handler.sh';
 
-// Execute the Bash script
+// Execute the Bash script and capture the output
 $output = shell_exec("bash $scriptPath 2>&1");
 
-// Display or log the output (for debugging)
+// Save the output to a log file
+$logFile = '../api-update-log.txt';
+file_put_contents($logFile, $output, FILE_APPEND); // Use FILE_APPEND to add new output without overwriting
+
+// Display the output (optional)
 echo "<pre>$output</pre>";
 ?>
