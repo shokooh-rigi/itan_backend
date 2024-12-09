defbec115064a22ae8866baae6d5640f47857407
Reza Dadashian <rezadadashian@yahoo.ca>
2024-12-09 10:14:01 +0330

    fix: webhook handler

--- webhook-handler.sh
+++ webhook-handler.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Navigate to your Django app's repository
+# Activate your virtual environment
 cd /home/airdec/public_html/api.airdec.net
+source /home/airdec/virtualenv/public_html/api.airdec.net/3.11/bin/activate
 
 # Pull the latest changes from the specific branch
 git stash --include-untracked
 git pull
 git stash apply
 
-# Activate your virtual environment
-source /home/airdec/virtualenv/public_html/api.airdec.net/3.11/bin/activate
 
 # Run Django management commands
-python manage.py makemigrations
+# python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
 
 touch /home/airdec/public_html/api.airdec.net/tmp/restart.txt
