bd9af5abddf6a430aeaff9130d4b98696dbe7030
Reza Dadashian <rezadadashian@yahoo.ca>
2025-01-15 10:20:50 +0330

    feat: add estimate resializer inside the proposal serializer

--- mysite/api/v2/proposal/serializers.py
+++ mysite/api/v2/proposal/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from mysite.api.v2.estimator.serializers import EstimateSerializer
 from mysite.proposal.models import Proposal
 
 
 class ProposalSerializer(serializers.ModelSerializer):
+    estimate = EstimateSerializer()
+
     class Meta:
         model = Proposal
-        fields = '__all__'
+        fields = "__all__"
