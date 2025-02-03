1c48964f555780365bd2b628f2c571aefaa47a5c
shokooh Rigi <shokoohrigi@yahoo.com>
2025-02-03 18:28:07 +0330

    add settlement delete api

--- mysite/api/v2/settlement/urls.py
+++ mysite/api/v2/settlement/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SettlementListView, SettlementCreateView
+from .views import SettlementListView, SettlementCreateView, SettlementDeleteView
 
 urlpatterns = [
     path('settlement/list/', SettlementListView.as_view(), name='settlement-list'),
     path('settlement/create/', SettlementCreateView.as_view(), name='settlement-create'),
-
+    path('settlement/<int:pk>/delete/', SettlementDeleteView.as_view(), name='settlement-delete'),
 ]
--- mysite/api/v2/settlement/views.py
+++ mysite/api/v2/settlement/views.py
@@ -1,10 +1,15 @@
 from django.db.models import Q
+from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from mysite import settings
+from mysite.scheduler.models import ScheduleTech
 from mysite.settlement.models import Settlement
 from .serializers import SettlementSerializer
 
@@ -47,3 +52,40 @@ class SettlementCreateView(CreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = SettlementSerializer
     queryset = Settlement.objects.all()
+
+
+class SettlementDeleteView(APIView):
+    """
+    API endpoint to soft delete a settlement by marking it as deleted and updating related objects.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        try:
+            settlement = Settlement.objects.get(pk=pk)
+        except Settlement.DoesNotExist:
+            raise NotFound("Settlement not found")
+
+        for settled_schedule in settlement.settledschedule_set.all():
+            settled_schedule.schedule.order.fully_settled = False
+            settled_schedule.schedule.order.save()
+            try:
+                schedule_tech = ScheduleTech.objects.get(
+                    schedule=settled_schedule.schedule,
+                    assigned_to_contractor=settlement.contractor,
+                )
+                schedule_tech.settlement = False
+                schedule_tech.save()
+            except ScheduleTech.DoesNotExist:
+                continue
+        for settled_maintenance in settlement.settledmaintenances_set.all():
+            settled_maintenance.maintenance.settlement = False
+            settled_maintenance.maintenance.save()
+
+        settlement.soft_delete()
+        return Response(
+            {"detail": "Settlement successfully deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
