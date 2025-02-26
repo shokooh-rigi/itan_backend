0affeb8daae32538febe3aafd523150681a3b16f
Reza Dadashian <rezadadashian@yahoo.ca>
2025-02-26 10:15:39 +0330

    feat: proxy enable on django manage.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import socks
+import socket
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+if settings.PROXY_ENABLE:
+    s = socks.socksocket()
+    socks.set_default_proxy(socks.SOCKS5, settings.PROXY_HOST, settings.PROXY_PORT)
+    socket.socket = socks.socksocket
 
 application = get_wsgi_application()
