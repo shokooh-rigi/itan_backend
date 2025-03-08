aaa3450d308d71d77ac4eb1202bd2e4a94b167c9
Shokooh Rigi <sh.rigi@wallex.net>
2025-03-08 16:30:23 +0330

    TechLabelRetrieveUpdateViewDeleteView

--- mysite/api/v2/order/views.py
+++ mysite/api/v2/order/views.py
@@ -43,7 +43,6 @@ from .services.order_field_drawing_service import OrderFieldDrawingService
 from .services.order_full_update_service import OrderFullUpdateService
 from .services.order_service import OrderService, OrderEditService
 from .services.order_site_pictures_service import OrderSitePicturesService
-from .services.tech_label_service import TechLabelServiceLayer
 from ..proposal.serializers import ProposalSerializer
 
 
