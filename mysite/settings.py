cc6606a900676ce82bd1966fd3be317487fc2295
Reza Dadashian <rezadadashian@yahoo.ca>
2025-03-25 16:39:13 +0330

    fix: cors middleware must be the first one

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,9 +144,9 @@ if TAB_SYSTEM:
 
 # Middleware
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
