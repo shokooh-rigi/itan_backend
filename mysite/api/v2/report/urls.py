b98e19103a2ee9b6c2e43c35c0581bea13419f59
Shokooh Rigi <sh.rigi@wallex.net>
2025-03-31 19:42:19 +0330

    add report urls

--- mysite/api/v2/report/urls.py
+++ mysite/api/v2/report/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from mysite.api.v2.coi.views import PerformanceListView
+from mysite.api.v2.report.views import PerformanceListView
 
 urlpatterns = [
     path('report/performance', PerformanceListView.as_view(), name='performance'),
