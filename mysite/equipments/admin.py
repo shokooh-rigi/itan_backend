5d1f78e6dee39feb2a18b0339c062211b762cbd9
Reza Dadashian <rezadadashian@yahoo.ca>
2025-04-09 19:51:40 +0330

    2

--- mysite/equipments/admin.py
+++ mysite/equipments/admin.py
@@ -3,17 +3,27 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-class TestSheetAdmin(admin.ModelAdmin):
+
+class TestSheetAdmin(ImportExportModelAdmin):
     model = TestSheet
-    list_display = ('id', 'name', 'created_on')
-    search_fields = ('id', 'name')
+    list_display = ("id", "name", "created_on")
+    search_fields = ("id", "name")
+
 
 admin.site.register(TestSheet, TestSheetAdmin)
 
 
-class EquipmentAdmin(admin.ModelAdmin):
+class EquipmentAdmin(ImportExportModelAdmin):
     model = Equipment
-    list_display = ('id', 'name', 'service', 'test_sheet', 'price', 'created_on',)
-    search_fields = ('id',)
+    list_display = (
+        "id",
+        "name",
+        "service",
+        "test_sheet",
+        "price",
+        "created_on",
+    )
+    search_fields = ("id",)
+
 
 admin.site.register(Equipment, EquipmentAdmin)
